a7a270ef15eebce443eda94657c861890784e45d
Jean <srjean21@gmail.com>
2026-03-01 16:07:29 -0400

    feat: changed details to btn primary variant on dark mode

--- unocss/shortcuts.ts
+++ unocss/shortcuts.ts
@@ -16,7 +16,7 @@ const btn = {
     "fc gap-x-2 rounded-field transition-[background-color,color,transform,border-color,outline-color] duration-200 ease-in-out cursor-pointer select-none",
 
   ["ui-btn/primary"]:
-    "squircle font-600 tracking-wide bg-primary pover:bg-primary/80 border-1 border-[#0002] text-primary-text shadow-[inset_0_2px_0px_#fff3,0_1px_4px_#0002]",
+    "squircle font-600 tracking-wide bg-primary pover:bg-primary/80 border-1 border-[#0002] text-primary-text shadow-[inset_0_2px_0px_#fff3,0_1px_4px_#0002] dark:shadow-[inset_0_2px_0px_#fff4,_0_1px_4px_#0008]",
 
   ["ui-btn/secondary"]:
     "squircle font-600 tracking-wide bg-secondary pover:bg-secondary/80 text-secondary-text",
